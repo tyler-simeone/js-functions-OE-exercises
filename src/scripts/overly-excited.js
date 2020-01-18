adcf406ed85077a52ede6d921109d77b642602b1
Tyler Simeone <tyler.simeone@gmail.com>
2020-01-18 15:40:11 -0600

    overly excited exercise part 4

--- src/scripts/overly-excited.js
+++ src/scripts/overly-excited.js
@@ -1,29 +1,27 @@
 // Create an array that contains the words in the sentence
 let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
 
-/*
-    The addExcitement function should be an impure function, and accept
-    the array as the sole argument. It should iterate over the array
-    and output the words to the browser console.
-*/
-function addExcitement (theWordArray, char) {
+function addExcitement (theWordArray, char, num) {
 
-    // Each time the for loop executes, you're going to add one more 
-    // word to this string
     let buildMeUp = "";
 
     for (let i = 0; i < theWordArray.length; i++) {
         if (i % 3 === 0 && i !== 0) {
-            buildMeUp += " " + theWordArray[i] + char;
+            buildMeUp += " " + theWordArray[i] + repeatChar(char, num);
         }
         else {
             buildMeUp += " " + theWordArray[i];
         }
         console.log(buildMeUp);
     }
-
+    
 };
 
-// Invoke the function and pass in the array
-addExcitement(sentence, "?");
-
+function repeatChar (char, num) {
+    let chars = "";
+    for (let i = 0; i < num; i++) {
+        chars += char;
+    }
+    return chars;
+}
+addExcitement(sentence, "?", 4);
